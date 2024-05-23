a1c7c7808087c7a02eca1f22c1c7324f6e42adc7
Fernando Depizol <luisfernando.depizol@gmail.com>
2024-05-22 23:18:30 -0300

    adicionado mais um livro

--- src/main/java/br/com/uniciv/rest/livraria/LivroRepositorio.java
+++ src/main/java/br/com/uniciv/rest/livraria/LivroRepositorio.java
@@ -12,6 +12,7 @@ public class LivroRepositorio {
 	public LivroRepositorio() {
 		Livro livro1= new Livro(1L, "Livro A", "ISBN-1234", "Genero A", 23.99, "Autor1");
 		Livro livro2= new Livro(2L, "Livro B", "ISBN-4321", "Genero B", 23.99, "Autor2");
+		Livro livro3= new Livro(3L, "Livro C", "ISBN-1324", "Genero C", 23.99, "Autor1");
 		
 		livros.put(livro1.getId(), livro1);
 		livros.put(livro2.getId(), livro2);
